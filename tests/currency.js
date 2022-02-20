a41c65537a25a5e2ff99a729022f2e05193425b2
I-Have-An-Issue <34550332+I-Have-An-Issue@users.noreply.github.com>
2022-02-20 15:26:32 -0500

    Use MIT license, add User.currency()

--- tests/currency.js
+++ tests/currency.js
@@ -2,15 +2,13 @@ const { username, password } = require("./creds.json")
 const Polygon = require("../src")
 const user = new Polygon.User(username, password)
 
-const status = "Dis is pretty cool"
-
 ;(async () => {
     if (!(await user.login())) return console.log(`[x] Login failed!`)
     const info = await user.info()
     console.log(`[✓] Login success: ${info.username} (${info.id})`)
 
-    console.log(`[?] Setting status to: ${status}`)
-    await user.setStatus(status)
+    const balance = await user.currency()
+    balance ? console.log(`[✓] ${balance} Pizzas`) : console.log(`[x] Cannot get balance!`)
 
     await user.logout() ? console.log(`[✓] Logout success!`) : console.log(`[x] Logout failed!`)
 })()
