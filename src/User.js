51eb8340a191781d54b1327e76ee3afca5aa8524
I-Have-An-Issue <34550332+I-Have-An-Issue@users.noreply.github.com>
2022-02-20 15:44:23 -0500

    .ping(), merge .currency() with .info(), .friendRequests

--- src/User.js
+++ src/User.js
@@ -1,4 +1,4 @@
-const { polygon, api } = require("../modules/polygon")
+const { polygon, api } = require("./modules/polygon")
 const cheerio = require("cheerio")
 
 class User {
@@ -51,23 +51,43 @@ class User {
 
     info() {
         return new Promise(async (resolve, reject) => {
+            let info = { username: "", id: 0, currency: 0 }
+
             polygon.get("/user").then((response) => {
                 if (response.status !== 200) return resolve(false)
                 const $ = cheerio.load(response.data)
                 const link = $(`a[class="nav-link mr-2"]`)
-                const username = link.text()
-                const id = new URL(`https://polygon.pizzaboxer.xyz${link.attr("href")}`).searchParams.get("ID")
-                resolve({ username, id })
+                info.username = link.text()
+                info.id = new URL(`https://polygon.pizzaboxer.xyz${link.attr("href")}`).searchParams.get("ID")
+            }).catch(e => reject(e))
+
+            api.get("/currency/balance").then((response) => {
+                if (response.status !== 200) return resolve(false)
+                info.currency = response.data[Object.keys(response.data)[0]]
+                resolve(info)
             }).catch(e => reject(e))
         })
     }
 
-    currency() {
+    ping() {
         return new Promise(async (resolve, reject) => {
-            api.get("/currency/balance").then((response) => {
-                if (response.status !== 200) return resolve(false)
-                resolve(response.data[Object.keys(response.data)[0]])
-            }).catch(e => reject(e))
+            this.#refreshCsrf().then(() => {
+                polygon.post("/api/account/update-ping").then((response) => {
+                    if (response.status !== 200) return resolve(false)
+                    resolve({ success: response.data.success, friendRequests: response.data.friendRequests })
+                }).catch(e => reject(e))
+            })
+        })
+    }
+
+    friendRequests() {
+        return new Promise(async (resolve, reject) => {
+            this.#refreshCsrf().then(() => {
+                polygon.post("/api/account/update-ping").then((response) => {
+                    if (response.status !== 200) return resolve(false)
+                    resolve(response.data.friendRequests)
+                }).catch(e => reject(e))
+            })
         })
     }
 }
--- tests/login_logout.js
+++ tests/login_logout.js
@@ -5,10 +5,13 @@ const user = new Polygon.User(username, password)
 ;(async () => {
     if (!(await user.login())) return console.log(`[x] Login failed!`)
     const info = await user.info()
-    console.log(`[✓] Login success: ${info.username} (${info.id})`)
+    console.log(`[✓] Login success: ${info.username} (${info.id}), ${info.currency} Pizzas`)
 
     console.log(`[?] Waiting 5 seconds.`)
     await new Promise(resolve => setTimeout(resolve, 5000))
+
+    const ping = await user.ping()
+    ping ? console.log(`[✓] Friend requests: ${ping.friendRequests}`) : console.log(`[x] Ping failed!`)
 
     await user.logout() ? console.log(`[✓] Logout success!`) : console.log(`[x] Logout failed!`)
 })()
